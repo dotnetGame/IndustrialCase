410b4cf5b2ecbba7abc94032d6df4572f1daa800
Jun Wang <1103870790@qq.com>
2021-08-08 17:23:42 +0800

    update recipe datagen

--- src/main/java/com/iteale/industrialcase/core/datagen/recipe/OresRecipes.java
+++ src/main/java/com/iteale/industrialcase/core/datagen/recipe/OresRecipes.java
@@ -6,15 +6,15 @@ import com.iteale.industrialcase.core.registries.BlockRegistry;
 import com.iteale.industrialcase.core.registries.ItemRegistry;
 import net.minecraft.advancements.critereon.InventoryChangeTrigger;
 import net.minecraft.data.DataGenerator;
-import net.minecraft.data.recipes.FinishedRecipe;
-import net.minecraft.data.recipes.RecipeProvider;
-import net.minecraft.data.recipes.ShapedRecipeBuilder;
+import net.minecraft.data.recipes.*;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.crafting.Ingredient;
 
 import java.util.function.Consumer;
 
-public class OresRecipes
-        extends RecipeProvider
+public class OresRecipes extends RecipeProvider
 {
     public OresRecipes(DataGenerator generatorIn) {
         super(generatorIn);
@@ -54,5 +54,50 @@ public class OresRecipes
                 .group(IndustrialCase.MODID)
                 .unlockedBy("item", InventoryChangeTrigger.TriggerInstance.hasItems(BlockRegistry.LEAD_BLOCK.get()))
                 .save(consumer, new ResourceLocation(IndustrialCase.MODID, "resource/ore/lead_ingot"));
+
+        ShapelessRecipeBuilder.shapeless(ItemRegistry.COPPER_PLATE.get())
+                .requires(Items.COPPER_INGOT)
+                .requires(ItemRegistry.FORGE_HAMMER.get())
+                .group(IndustrialCase.MODID)
+                .unlockedBy("item", InventoryChangeTrigger.TriggerInstance.hasItems(Items.COPPER_INGOT, ItemRegistry.FORGE_HAMMER.get()))
+                .save(consumer, new ResourceLocation(IndustrialCase.MODID, "resource/plate/copper_plate"));
+
+        ShapelessRecipeBuilder.shapeless(ItemRegistry.LEAD_PLATE.get())
+                .requires(ItemRegistry.LEAD_INGOT.get())
+                .requires(ItemRegistry.FORGE_HAMMER.get())
+                .group(IndustrialCase.MODID)
+                .unlockedBy("item", InventoryChangeTrigger.TriggerInstance.hasItems(ItemRegistry.LEAD_INGOT.get(), ItemRegistry.FORGE_HAMMER.get()))
+                .save(consumer, new ResourceLocation(IndustrialCase.MODID, "resource/plate/lead_plate"));
+
+        ShapelessRecipeBuilder.shapeless(ItemRegistry.TIN_PLATE.get())
+                .requires(ItemRegistry.TIN_INGOT.get())
+                .requires(ItemRegistry.FORGE_HAMMER.get())
+                .group(IndustrialCase.MODID)
+                .unlockedBy("item", InventoryChangeTrigger.TriggerInstance.hasItems(ItemRegistry.TIN_INGOT.get(), ItemRegistry.FORGE_HAMMER.get()))
+                .save(consumer, new ResourceLocation(IndustrialCase.MODID, "resource/plate/tin_plate"));
+
+        ShapelessRecipeBuilder.shapeless(ItemRegistry.IRON_PLATE.get())
+                .requires(Items.IRON_INGOT)
+                .requires(ItemRegistry.FORGE_HAMMER.get())
+                .group(IndustrialCase.MODID)
+                .unlockedBy("item", InventoryChangeTrigger.TriggerInstance.hasItems(Items.IRON_INGOT, ItemRegistry.FORGE_HAMMER.get()))
+                .save(consumer, new ResourceLocation(IndustrialCase.MODID, "resource/plate/iron_plate"));
+
+        ShapelessRecipeBuilder.shapeless(ItemRegistry.GOLD_PLATE.get())
+                .requires(Items.GOLD_INGOT)
+                .requires(ItemRegistry.FORGE_HAMMER.get())
+                .group(IndustrialCase.MODID)
+                .unlockedBy("item", InventoryChangeTrigger.TriggerInstance.hasItems(Items.GOLD_INGOT, ItemRegistry.FORGE_HAMMER.get()))
+                .save(consumer, new ResourceLocation(IndustrialCase.MODID, "resource/plate/gold_plate"));
+
+        SimpleCookingRecipeBuilder.smelting(Ingredient.of(BlockRegistry.LEAD_ORE.get()), ItemRegistry.LEAD_INGOT.get(), 0.7F, 200)
+                .group(IndustrialCase.MODID)
+                .unlockedBy("item", InventoryChangeTrigger.TriggerInstance.hasItems(BlockRegistry.LEAD_ORE.get()))
+                .save(consumer, new ResourceLocation(IndustrialCase.MODID, "resource/ingot/lead_ingot_from_smelting"));
+
+        SimpleCookingRecipeBuilder.smelting(Ingredient.of(BlockRegistry.TIN_ORE.get()), ItemRegistry.TIN_INGOT.get(), 0.7F, 200)
+                .group(IndustrialCase.MODID)
+                .unlockedBy("item", InventoryChangeTrigger.TriggerInstance.hasItems(BlockRegistry.TIN_ORE.get()))
+                .save(consumer, new ResourceLocation(IndustrialCase.MODID, "resource/ingot/tin_ingot_from_smelting"));
     }
 }
---
+++ src/main/java/com/iteale/industrialcase/core/datagen/recipe/ToolRecipes.java
@@ -0,0 +1,33 @@
+package com.iteale.industrialcase.core.datagen.recipe;
+
+import com.iteale.industrialcase.core.IndustrialCase;
+import com.iteale.industrialcase.core.registries.BlockRegistry;
+import com.iteale.industrialcase.core.registries.ItemRegistry;
+import net.minecraft.advancements.critereon.InventoryChangeTrigger;
+import net.minecraft.data.DataGenerator;
+import net.minecraft.data.recipes.*;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.item.crafting.SmeltingRecipe;
+
+import java.util.function.Consumer;
+
+public class ToolRecipes extends RecipeProvider {
+    public ToolRecipes(DataGenerator generatorIn) {
+        super(generatorIn);
+    }
+
+    @Override
+    protected void buildCraftingRecipes(Consumer<FinishedRecipe> consumer) {
+        ShapelessRecipeBuilder.shapeless(BlockRegistry.RUBBER_PLANKS.get(), 4)
+                .requires(BlockRegistry.RUBBER_LOG.get())
+                .group(IndustrialCase.MODID)
+                .unlockedBy("item", InventoryChangeTrigger.TriggerInstance.hasItems(BlockRegistry.RUBBER_LOG.get()))
+                .save(consumer, new ResourceLocation(IndustrialCase.MODID, "resource/plant/rubber_planks"));
+
+        SimpleCookingRecipeBuilder.smelting(Ingredient.of(ItemRegistry.RESIN.get()), ItemRegistry.RUBBER.get(), 0.7F, 200)
+                .group(IndustrialCase.MODID)
+                .unlockedBy("item", InventoryChangeTrigger.TriggerInstance.hasItems(ItemRegistry.RESIN.get()))
+                .save(consumer, new ResourceLocation(IndustrialCase.MODID, "crafting/rubber_from_smelting"));
+    }
+}
